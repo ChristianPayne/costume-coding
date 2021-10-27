433688d8f1ca4036f6da168cc3c28ddca6b7328d
Christian Payne <christian.p@modelmatch.com>
2021-10-27 11:07:45 -0700

    Further working on others' costumes.

--- code.py
+++ code.py
@@ -5,9 +5,21 @@ import math
 import random
 
 pixel_pin = board.D24
-num_pixels = 6
+num_pixels = 19 + 12
+# num_pixels = 6
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.25, auto_write=False, pixel_order=(1, 0, 2))
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=False, pixel_order=(1, 0, 2))
+
+PURPLE = (75,0,128)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0, 0, 255)
+ORANGE = (255, 40, 0)
+NEON_GREEN = (255, 255, 0)
+YELLOW = (255, 180, 0)
+CYAN = (0, 255, 255)
+
+COLOR = PURPLE
 
  
 def grad(pos):
@@ -37,16 +49,16 @@ def wheel(pos):
     return (pos * 3, 0, 255 - pos * 3)
 
 def chase (frameNumber: int, pixelGroup: list[int], totalFrames: int):
-    tailLength = 0
+    tailLength = 3
     dotColor = (255, 255, 255)
-    tailColor = (0, 0, 255)
+    tailColor = (0, 255, 0)
 
     framePercent = frameNumber / totalFrames
 
-    pixels.fill((0,0,0))
-    if(frameNumber == 0):
-        for pixel in pixelGroup:
-            pixels[pixel] = (0,0,0)
+    pixels.fill((0,255,255))
+    # if(frameNumber == 0):
+    #     for pixel in pixelGroup:
+    #         pixels[pixel] = (0,0,0)
     for pixel in range(len(pixelGroup)):
         if(pixel == int(framePercent * len(pixelGroup))):
             pixels[pixelGroup[pixel]] = dotColor
@@ -81,9 +93,9 @@ def sin (frameNumber: int, pixelGroup: list[int], totalFrames: int):
     for pixel in pixelGroup:
         pixels[pixel] = (int(255 * sinWave), 0, 0)
 
-def solid_color (frameNumber: int, pixelGroup: list[int]):
+def solid_color (frameNumber: int, pixelGroup: list[int], totalFrames: int):
     for pixel in pixelGroup:
-        pixels[pixel] = (0, 255, 255)
+        pixels[pixel] = COLOR
 
 def rainbow_cycle(frameNumber: int, pixelGroup: list[int], totalFrames: int):
     framePercent = frameNumber / totalFrames
@@ -178,14 +190,23 @@ def StrandTest ():
 
 shoulderPixels = [0,1,2,3,4,5]
 additionalPixels = [6,7,8,9,10,11]
+chestPixels = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+baddieShoulderPixels = [19,20,21,22,23,24,25,26,27,28,29,30]
 # shoulderPixels = shoulderPixels + additionalPixels
 # collarPixels = [6,7,8,9,10,11,12,13,14]
 # allPixels = shoulderPixels + collarPixels
 
 while True:
     RunAnimation([
-        (start_up_animation, shoulderPixels)
-        ], 10, 100, 10)
+        # (start_up_animation, shoulderPixels),
+        (solid_color, chestPixels + baddieShoulderPixels),
+        (glitch, chestPixels + baddieShoulderPixels),
+        ], 30, 300, 10)
+    RunAnimation([
+        # (start_up_animation, shoulderPixels),
+        (rainbow_cycle, chestPixels + baddieShoulderPixels),
+        # (glitch, chestPixels),
+        ], 30, 300, 10)
     # RunAnimation(animation2,5)
     # RepeatAnimation(rainbow_cycle, 5)
     # StrandTest()
